2bc61779ad421d19852f92eed2e5aa19537bb101
misieksnk <michal@brygidyn.pl>
2016-06-06 23:14:56 +0200

    First attempt

---
+++ src/MisiekSnk/PoManager/PoManager.php
@@ -0,0 +1,96 @@
+<?php
+namespace MisiekSnk\PoManager;
+
+use MsgFmt\Generate;
+use Sepia\FileHandler;
+use Sepia\PoParser;
+
+class PoManager
+{
+    private $poFile;
+    private $parser;
+    private $parsedTranslations = [];
+    private $translations = [];
+    private $parsedKeys = [];
+
+    /**
+     * PoManager constructor.
+     * @param $filename string Path to .po file
+     */
+    public function __construct($filename)
+    {
+        $this->poFile = $filename;
+        $fileHandler = new FileHandler($this->poFile);
+        $this->parser = new PoParser($fileHandler);
+        $this->fillArrays();
+    }
+
+    /**
+     * @return array
+     */
+    public function getTranslationsArray()
+    {
+        return $this->translations;
+    }
+
+    /**
+     * @param $msgId string Phase to translate
+     * @return bool|string
+     */
+    public function getTranslation($msgId)
+    {
+        return !empty($this->translations[$msgId]) ? $this->translations[$msgId] : false;
+    }
+
+    /**
+     * @param $msgId string Phrase to translate
+     * @param $msgStr string Translated phrase
+     * @throws \Exception
+     */
+    public function setTranslation($msgId, $msgStr)
+    {
+        $this->translations[$msgId] = $msgStr;
+        $this->parsedTranslations[$this->parsedKeys[$msgId]]['msgstr'] = explode(PHP_EOL, $msgStr);
+        $this->parser->setEntry($this->parsedKeys[$msgId], $this->parsedTranslations[$this->parsedKeys[$msgId]]);
+        $this->parser->writeFile($this->poFile);
+    }
+
+    /**
+     * Updates .mo file for current .po file
+     */
+    public function updateMo()
+    {
+        $moWriter = new Generate();
+        $moWriter->convert($this->poFile);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function fillArrays()
+    {
+        $this->parsedTranslations = $this->parser->parse();
+
+        $translations = [];
+        $parsedKeys = [];
+        foreach ($this->parsedTranslations as $key => $parsedTranslation) {
+            $niceKey = $this->getNiceKey($key);
+            $parsedKeys[$niceKey] = $key;
+            $translations[$niceKey] = trim(join(PHP_EOL, $parsedTranslation['msgstr']));
+        }
+
+        $this->translations = $translations;
+        $this->parsedKeys = $parsedKeys;
+    }
+
+    /**
+     * @param $dirtyKey string Array key from Sepia array
+     * @return string
+     */
+    private function getNiceKey($dirtyKey)
+    {
+        $niceKey = preg_replace('!\s+!', ' ', trim(str_replace([PHP_EOL, '<##EOL##>'], [' ', ' '], $dirtyKey)));
+
+        return $niceKey;
+    }
+}
---
+++ tests/PoManagerTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace MisiekSnk\PoManager;
+
+/**
+ * Class PoManagerTest
+ */
+class PoManagerTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        copy(dirname(__FILE__) . '/translations/pl.default.po', dirname(__FILE__) . '/translations/pl.po');
+    }
+    
+    public function tearDown()
+    {
+        unlink(dirname(__FILE__) . '/translations/pl.po');
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldReturnTranslationsArray()
+    {
+        $poManager = new PoManager(dirname(__FILE__) . '/translations/pl.po');
+
+        $transltions = $poManager->getTranslationsArray();
+        $this->assertInternalType('array', $transltions);
+        $this->assertArrayHasKey('Phrase with no translation', $transltions);
+    }
+
+    /**
+     * @test
+     * @dataProvider dpItShouldReturnTranslation
+     */
+    public function itShouldReturnTranslation($phrase, $expected)
+    {
+        $poManager = new PoManager(dirname(__FILE__) . '/translations/pl.po');
+
+        $translation = $poManager->getTranslation($phrase);
+        $this->assertEquals($expected, $translation);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldChangeTranslation()
+    {
+        $poManager = new PoManager(dirname(__FILE__) . '/translations/pl.po');
+
+        $phrase = 'Phrase with no translation';
+        $translated = 'Fraza bez translacji';
+        $poManager->setTranslation($phrase, $translated);
+
+        $translation = $poManager->getTranslation($phrase);
+
+        $this->assertEquals($translated, $translation);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldGenerateMoFile()
+    {
+        $poManager = new PoManager(dirname(__FILE__) . '/translations/pl.po');
+        $poManager->updateMo();
+
+        $this->assertTrue(file_exists(dirname(__FILE__) . '/translations/pl.mo'));
+
+        unlink(dirname(__FILE__) . '/translations/pl.mo');
+    }
+
+    //================================
+
+    public function dpItShouldReturnTranslation()
+    {
+        return [
+            ['Phrase with no translation', ''],
+            ['Simple phrase', 'Prosta fraza'],
+            ['Multiline phrase To translate', 'Wielolinijkowa fraza' . PHP_EOL . PHP_EOL . 'Do tłumaczenia'],
+            ['Not exists', false]
+        ];
+    }
+}
